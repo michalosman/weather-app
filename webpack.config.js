e0d3fc6620e25978534316abadfb13008632c3e6
michalosman <michal.osman.997@gmail.com>
2021-02-19 19:59:33 +0100

    Setup tools

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
