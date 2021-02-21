2d64f228fad46432f5f4559b6d939a355b882734
michalosman <michal.osman.997@gmail.com>
2021-02-21 20:09:19 +0100

    Finish project

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -10,10 +10,9 @@ const weather = (() => {
 
   async function getData(city) {
     const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=28fe7b5f9a78838c639143fc517e4343`;
-
     try {
       const response = await fetch(endpoint, { mode: "cors" });
-      if (!response.ok) throw new Error("City not found");
+      if (!response.ok) throw new Error(`City ${city} not found`);
       const data = convertData(await response.json());
       return data;
     } catch (error) {
@@ -21,7 +20,6 @@ const weather = (() => {
       return null;
     }
   }
-
   return { getData };
 })();
 
